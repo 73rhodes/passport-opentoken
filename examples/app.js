d4fd72c878f405ae7d18226fd15b4d5668e2d306
Darren DeRidder <drderidder@gmail.com>
2014-10-10 16:31:04 -0400

    Update example; work in progress

--- examples/app.js
+++ examples/app.js
@@ -14,11 +14,6 @@ var otkOptions = {
 
 // Options for PingFederate
 var pfOptions = {
-  host: "qa-test",
-  port: 9031,
-  https: true,
-  path: "/idp/startSSO.ping",
-  partnerSpId: "PF-DEMO",
   targetResource: "http://localhost:3000/ssolanding",
   tokenName: 'opentoken',
   cookiePath: '/',
@@ -27,20 +22,16 @@ var pfOptions = {
 
 var api = new opentoken.OpenTokenAPI(otkCipherSuite, otkPassword, otkOptions);
 
-app.get('/mobile/singlesignon', function (req, res) {
-  var protocol = pfOptions.https ? "https://" : "http://";
-  var host     = pfOptions.host || "localhost";
-  var port     = pfOptions.port || 80;
-  var path     = pfOptions.path || "/idp/startSSO.ping";
-  var spId     = pfOptions.partnerSpId || "PF-DEMO";
-  var target   = pfOptions.targetResource || "http://localhost/sso";
-  var ssoUrl   = protocol + host + ":" + port + path;
-  var ssoQuery = "?PartnerSpId=" + spId + "&TargetResource=" + target; 
-  res.status(302).redirect(ssoUrl + ssoQuery);
+app.get('/login/opentoken', function (req, res) {
+  var ssoUrl = "https://localhost:9031" + 
+    "/idp/startSSO.ping" +
+    "?PartnerSpId=PF-DEMO" + 
+    "&TargetResource=https://localhost:3000/login/opentoken/callback";
+  res.status(302).redirect(ssoUrl);
 });
 
-app.get('/mobile/ssolanding', function (req, res) {
-  var token = req.param(pfOptions.tokenName); // 'kssubportal'
+app.get('/login/opentoken/callback', function (req, res) {
+  var token = req.param(pfOptions.tokenName); 
   console.log(token);
   api.parseToken(token, function (err, data) {
     if (err) {
@@ -49,14 +40,14 @@ app.get('/mobile/ssolanding', function (req, res) {
     } else {
       console.log(data);
       res.status(200).send(
-       '<html><a href="/mobile/singlelogout">logout</a><br><pre>' + 
+       '<html><a href="/logout/opentoken">logout</a><br><pre>' + 
        JSON.stringify(data, null, 2) + '</pre></html>');
     }
   });
 });
 
-app.get('/mobile/singlelogout', function (req, res) {
-  res.status(302).redirect('https://qa-test:9031/quickstart-app-idp/go?action=logout');
+app.get('/logout/opentoken', function (req, res) {
+  res.status(302).redirect('https://localhost:9031/quickstart-app-idp/go?action=logout');
 });
 
 var server = app.listen(3000, function () {
